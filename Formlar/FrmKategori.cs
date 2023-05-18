71b1fa342bde7791811b94c75dbcabf78e1b0807
salihkaplan44 <kaplan441993@gmail.com>
2023-05-18 22:48:58 +0300

    ekranların tasarımı yapıldı

---
+++ Formlar/FrmKategori.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeknikServis.Formlar
+{
+    public partial class FrmKategori : Form
+    {
+        public FrmKategori()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Formlar/FrmUrunSatis.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeknikServis.Formlar
+{
+    public partial class FrmUrunSatis : Form
+    {
+        public FrmUrunSatis()
+        {
+            InitializeComponent();
+        }
+
+
+        private void PictureClose_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureClose_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnVazgeç_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BrnKaydet_Click(object sender, EventArgs e)
+        {
+           
+        }
+       
+    private void BtnTutarHesapla_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lookUpEditUrun_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
